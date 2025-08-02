49cdef995e93ef5b00246d9a65dae9932ec8a95a
jaewon112 <ghfjf112@gmail.com>
2025-08-02 17:38:53 +0900

    헤더 사이드바 css

--- nomore_front/src/Layout/MainLayout/styles.js
+++ nomore_front/src/Layout/MainLayout/styles.js
@@ -7,22 +7,158 @@ export const root = css`
 `;
 
 export const header = css`
+  display: flex;
+  align-items: center;
+  border: 0.1rem solid #dbdbdb;
+  margin-bottom: 1rem;
   width: 100%;
-  height: 7rem;
-  background-color: #3f6583;
+  height: 6rem;
+  background-color: #fafafa;
+`;
+
+export const logo = css`
+  margin-left: 3rem;
+  & > h1 {
+    margin: 0;
+  }
+
+  & > h4 {
+    display: flex;
+    justify-content: center;
+    margin: 0;
+  }
+`;
+
+export const searchContainer = css`
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+  height: 4.1rem;
+  
+  & > button {
+    border-radius: 1rem;
+  }
+
+  & > input {
+    padding: 1rem;
+    border-radius: 1rem;
+    margin-left: 1.2rem;
+    box-sizing: border-box;
+    width: 59rem;
+    height: 3.6rem;
+  }
 `;
 
 export const body = css`
   display: flex;
-  flex: 1; /* header 제외한 남은 공간 채우기 */
+  flex-grow: 1; 
 `;
 
 export const leftSideBar = css`
-  width: 22rem; /* 원하는 만큼 줄이기 가능 */
-  background-color: green;
+  margin-left: 1rem;
+  padding: 2rem;
+  border: 0.1rem solid #dbdbdb;
+  border-radius: 1rem;
+  height: 100%;
+  width: 20rem;
+  background-color: #fafafa;
+  overflow-y: auto;
+
+  &::-webkit-scrollbar {
+    width: 0;
+  }
+`;
+
+export const loginContainer = css`
+  height: auto;
+  padding: 1.5rem;
+  border-radius: 1rem;
+  background: linear-gradient(to bottom right, #7e57c2, #673ab7);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  color: white;
+
+  & > h2 {
+    margin: 0;
+    font-size: 1.6rem;
+  }
+
+  & > button {
+    width: 16rem;
+    height: 3.5rem;
+    border: none;
+    border-radius: 0.8rem;
+    font-size: 1.5rem;
+    font-weight: 400;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.5rem;
+    cursor: pointer;
+    font-weight: 600;
+  }
+
+  & > button:nth-of-type(1) {
+    background-color: white;
+    color: black;
+  }
+
+  & > button:nth-of-type(2) {
+    background-color: #fee500;
+    color: #000000;
+  }
+`;
+
+export const sideMenu = css`
+  display: flex;
+  margin-top: 2rem;
+  flex-direction: column;
+  gap: 1rem;
+
+  & > button {
+    display: flex;
+    align-items: center;
+    gap: 0.8rem;
+    padding: 0.8rem;
+    border: none;
+    background-color: #fafafa;
+    border-radius: 0.8rem;
+    font-size: 1.4rem;
+    font-weight: 600;
+    cursor: pointer;
+    transition: background-color 0.2s;
+
+    &:hover {
+      background-color: #fafafa;
+    }
+
+    & > svg {
+      font-size: 1.8rem;
+    }
+  }
+`;
+
+export const sideCategory = css`
+  
+  & > button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 0.1rem solid #fafafa;
+    margin: 1rem 0;
+    gap: 0.9rem; 
+    width: 100%;
+    font-size: 1.7rem;
+    font-weight: 400;
+    background-color: #fafafa;
+    cursor: pointer;
+  }
 `;
 
 export const content = css`
-  flex: 1;
+  display: flex;
+  flex-grow: 1;
   background-color: lightblue;
 `;
--- nomore_front/src/pages/home/Home.jsx
+++ nomore_front/src/pages/home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Home(props) {
     return (
         <div>
-            
+            <h1>sadfgadf</h1>
         </div>
     );
 }
