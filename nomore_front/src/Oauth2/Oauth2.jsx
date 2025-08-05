98918b100239828e6b42661ae2f286e699ec363f
jaewon112 <ghfjf112@gmail.com>
2025-08-05 16:06:51 +0900

    회원가입페이지 수정

--- nomore_front/src/Oauth2/Oauth2.jsx
+++ nomore_front/src/Oauth2/Oauth2.jsx
@@ -1,19 +1,49 @@
 /** @jsxImportSource @emotion/react */
+import { useQueryClient } from '@tanstack/react-query';
 import * as s from './styles';
 import React from 'react';
 import { FcGoogle } from 'react-icons/fc';
 import { SiKakaotalk } from 'react-icons/si';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { reqFindUser } from '../api/authApi';
 
 function Oauth2(props) {
+    const [ searchParams ] = useSearchParams();
+    const navigate = useNavigate();
+    const queryClient = useQueryClient();
 
     const handleGoogleOnCLick = () => {
         window.location.href = "http://localhost:8080/oauth2/authorization/google"
+        const findUser = reqFindUser();
+        if (!!findUser) {
+            const accessToken = searchParams.get("accessToken");
+            localStorage.setItem("AccessToken", `Bearer ${accessToken}`)
+            queryClient.invalidateQueries({
+                queryKey: ["principal"]
+            }).then(() => {
+                navigate("/")
+            })
+        } else {
+            navigate("/auth/signup")
+        }
     }
-
+    
     const handleKakaoOnClick = () => {
         window.location.href = "http://localhost:8080/oauth2/authorization/kakao"
+        const findUser = reqFindUser();
+        if (!!findUser) {
+            const accessToken = searchParams.get("accessToken");
+            localStorage.setItem("AccessToken", `Bearer ${accessToken}`)
+            queryClient.invalidateQueries({
+                queryKey: ["principal"]
+            }).then(() => {
+                navigate("/")
+            })
+        } else {
+            navigate("/auth/signup")
+        }
     }
-
+    
     return (
         <div css={s.loginContainer}>
             <button onClick={handleGoogleOnCLick}><FcGoogle />구글 로그인</button>
