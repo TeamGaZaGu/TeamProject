8b9e5e5a3ba1e7e9dcd56373bc0ddbea4b4abc06
Deaung <a76650051@gmail.com>
2025-08-14 16:15:28 +0900

    20250814 게시판 수정 및 삭제 1차 완, 게시판 권한관련 수정

--- nomore_back/src/main/java/com/korit/nomoreback/domain/forum/ForumImgMapper.java
+++ nomore_back/src/main/java/com/korit/nomoreback/domain/forum/ForumImgMapper.java
@@ -1,6 +1,7 @@
 package com.korit.nomoreback.domain.forum;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -9,5 +10,8 @@ public interface ForumImgMapper {
 
     int insertImg(ForumImg forumImg);
     int insertMany(List<ForumImg> forumImgs);
+    ForumImg modifyImg(List<ForumImg> forumImgs);
+    List<ForumImg> findImgById(@Param("forumId") Integer forumId);
+    int deleteImg(@Param("imgIds")List<Integer> imgIds);
 
 }
